ce6821cd8fc0afe11c0e3573afcc1a56577bd09a
Yoshi Askharoun <jjask7@gmail.com>
2025-02-27 16:43:06 -0600

    Complete first-pass implementation

--- src/Nomad/ModifiableAccentColor.cs
+++ src/Nomad/ModifiableAccentColor.cs
@@ -1,13 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using CommunityToolkit.Diagnostics;
-using Ipfs;
-using Ipfs.CoreApi;
+﻿using Ipfs;
 using OwlCore.ComponentModel;
 using OwlCore.Kubo;
 using OwlCore.Nomad;
 using OwlCore.Nomad.Kubo;
-using OwlCore.Storage;
 using WindowsAppCommunity.Sdk.Models;
 
 namespace WindowsAppCommunity.Sdk.Nomad;
@@ -37,24 +32,70 @@ public class ModifiableAccentColor : NomadKuboEventStreamHandler<ValueUpdateEven
         if (updateEvent.TargetId != Id)
             return;
 
-        Inner.AccentColor = updateEvent.Value;
+        string? accentColor = null;
+        
+        if (updateEvent is { Unset: false, Value: not null })
+        {
+            (accentColor, _) = await Client.ResolveDagCidAsync<string>(updateEvent.Value!,  nocache: !KuboOptions.UseCache, cancellationToken);
+        }
+        
+        await ApplyEntryUpdateAsync(updateEvent, accentColor, cancellationToken);
+    }
+    
+    /// <summary>
+    /// Applies an event stream update event and raises the relevant events.
+    /// </summary>
+    /// <param name="updateEvent">The update event to apply.</param>
+    /// <param name="accentColor">The resolved accent color data for this event.</param>
+    /// <param name="cancellationToken">A token that can be used to cancel the ongoing operation.</param>
+    public Task ApplyEntryUpdateAsync(ValueUpdateEvent updateEvent, string? accentColor, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        if (updateEvent.EventId is not nameof(UpdateAccentColorAsync))
+            return Task.CompletedTask;
+        
+        Inner.Inner.AccentColor = accentColor;
+        AccentColorUpdated?.Invoke(this, accentColor);
+        
+        return Task.CompletedTask;
     }
 
-    /// <inheritdoc />
-    public override Task<EventStreamEntry<Cid>> AppendNewEntryAsync(ValueUpdateEvent updateEvent, CancellationToken cancellationToken = new CancellationToken())
+    /// <inheritdoc cref="INomadKuboEventStreamHandler{TEventEntryContent}.AppendNewEntryAsync" />
+    public override async Task<EventStreamEntry<Cid>> AppendNewEntryAsync(ValueUpdateEvent updateEvent, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        // Use extension method for code deduplication (can't use inheritance).
+        var localUpdateEventCid = await Client.Dag.PutAsync(updateEvent, pin: KuboOptions.ShouldPin, cancel: cancellationToken);
+        var newEntry = await this.AppendEventStreamEntryAsync(localUpdateEventCid, updateEvent.EventId, updateEvent.TargetId, cancellationToken);
+        return newEntry;
     }
 
     /// <inheritdoc />
     public override Task ResetEventStreamPositionAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        EventStreamPosition = null;
+        Inner.Inner.AccentColor = null;
+            
+        return Task.CompletedTask;
     }
     
     /// <inheritdoc />
-    public Task UpdateAccentColorAsync(string? accentColor, CancellationToken cancellationToken)
+    public async Task UpdateAccentColorAsync(string? accentColor, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        bool unset = accentColor is null;
+
+        DagCid? valueCid = null;
+        if (!unset)
+        {
+            Cid cid = await Client.Dag.PutAsync(accentColor!, pin: KuboOptions.ShouldPin, cancel: cancellationToken);
+            valueCid = (DagCid)cid;
+        }
+
+        var updateEvent = new ValueUpdateEvent(Id, nameof(UpdateAccentColorAsync), null, valueCid, unset);
+
+        await ApplyEntryUpdateAsync(updateEvent, accentColor, cancellationToken);
+        var appendedEntry = await AppendNewEntryAsync(updateEvent, cancellationToken);
+
+        EventStreamPosition = appendedEntry;
     }
 }
